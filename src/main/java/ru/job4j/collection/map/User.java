8cc3fd4795e8fda0c5cc5da0b3a4f8ed9b3e1376
WindzoR <windzr67@gmail.com>
2021-03-23 22:35:16 +0300

    4. С переопределением equals и hashCode [#475509]

--- src/main/java/ru/job4j/collection/map/User.java
+++ src/main/java/ru/job4j/collection/map/User.java
@@ -1,6 +1,7 @@
 package ru.job4j.collection.map;
 
 import java.util.Calendar;
+import java.util.Objects;
 
 public class User {
     private String name;
@@ -20,5 +21,18 @@ public class User {
                 + ", children= " + children
                 + ", birthday= " + birthday.getTime()
                 + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name) && birthday.equals(user.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
     }
 }
