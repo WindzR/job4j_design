9cb6cde6975b848e07a236ed43ebf47777b8eb24
WindzoR <windzr67@gmail.com>
2021-03-30 15:58:51 +0300

    0.3. BufferedReader.(каркас) [#475526]

---
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -0,0 +1,14 @@
+package ru.job4j.io;
+
+import java.util.List;
+
+public class LogFilter {
+    public static List<String> filter(String file) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        List<String> log = filter("log.txt");
+        System.out.println(log);
+    }
+}
