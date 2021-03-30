13e1fce2ff62c9086e24dde1022ef1ce3826f40f
WindzoR <windzr67@gmail.com>
2021-03-30 14:35:32 +0300

    0.2. FileInputStream [#475535]

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -1,6 +1,8 @@
 package ru.job4j.io;
 
 import java.io.FileInputStream;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class EvenNumberFile {
     public static void main(String[] args) {
@@ -23,4 +25,22 @@ public class EvenNumberFile {
             ex.printStackTrace();
         }
     }
+
+//    public static void main(String[] args) {
+//        try (FileInputStream in = new FileInputStream("even.txt")) {
+//            StringBuilder nums = new StringBuilder();
+//            int read;
+//            while ((read = in.read()) != -1) {
+//                nums.append((char) read);
+//            }
+//            String[] numbers = nums.toString().split(System.lineSeparator());
+//            Arrays.stream(numbers)
+//                    .map(Integer::parseInt)
+//                    .filter(element -> element % 2 != 0)
+//                    .peek(element -> System.out.println("Число " + element + " является нечетным!"))
+//                    .collect(Collectors.toList());
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//        }
+//    }
 }
