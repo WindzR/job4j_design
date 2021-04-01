e566909191d002680d22dc9cba6dc3467f5c6c20
WindzoR <windzr67@gmail.com>
2021-04-02 00:53:51 +0300

    1. Читаем файл конфигурации [#475531] with tests

--- src/test/java/ru/job4j/io/ConfigTest.java
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -13,4 +13,24 @@ public class ConfigTest {
         config.load();
         assertThat(config.value("name"), is("Petr Arsentev"));
     }
+
+    @Test
+    public void whenPairWithCommentsAndEmptyLines() {
+        String path = "./data/pair_with_comments.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.dialect"), is("org.hibernate.dialect.PostgreSQLDialect"));
+    }
+
+    @Test
+    public void whenPairWithoutEmptyLines() {
+        String path = "app.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.dialect"), is("org.hibernate.dialect.PostgreSQLDialect"));
+        assertThat(config.value("hibernate.connection.url"), is("jdbc:postgresql://127.0.0.1:5432/trackstudio"));
+        assertThat(config.value("hibernate.connection.driver_class"), is("org.postgresql.Driver"));
+        assertThat(config.value("hibernate.connection.username"), is("postgres"));
+        assertThat(config.value("hibernate.connection.password"), is("password"));
+    }
 }
