2ebdc67affecac16b21d516371b1e3969437ccc7
WindzR <bodins@yandex.ru>
2021-04-08 00:08:42 +0300

    5.1. Именованные аргументы [#475529] Without IllegalArgumentException

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,36 @@
+package ru.job4j.io;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        /* TODO parse args to values. */
+        String[] temp = new String[2];
+        for (int i = 0; i < args.length; i++) {
+            temp = args[i].split("=");
+            temp[0] = temp[0].substring(1);
+            values.put(temp[0], temp[1]);
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
