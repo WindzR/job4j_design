731b6d6e13221987a6538b9b2f24572387dcfb21
WindzoR <windzr67@gmail.com>
2021-04-10 18:14:09 +0300

    5.2. Архивировать проект [#475533] архивация папки

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -1,6 +1,7 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -10,6 +11,15 @@ public class Zip {
     public void packFiles(List<File> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
             for (File file : sources) {
+                if (file.isDirectory()) {
+                    System.out.println("Adding directory <" + file.getName() + "> to archive");
+                    File[] filesInDir = file.listFiles();
+                    if (filesInDir != null) {
+                        List<File> files = Arrays.asList(filesInDir);
+                        packFiles(files, target);
+                        continue;
+                    }
+                }
                 System.out.println("Adding file <" + file.getName() + "> to archive");
                 zip.putNextEntry(new ZipEntry(file.getPath()));
                 try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(file))) {
@@ -35,9 +45,7 @@ public class Zip {
 
     public static void main(String[] args) {
         new Zip().packFiles(List.of(
-                new File("checkstyle.xml"),
-                new File("pom.xml"),
-                new File("even.txt")
+                new File("TestForZip")
         ), new File("./archive.zip")
         );
     }
