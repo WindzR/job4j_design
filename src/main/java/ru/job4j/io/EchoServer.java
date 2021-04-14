ca54176db57a4ab8716cc349ff8e8595008ab78e
WindzoR <windzr67@gmail.com>
2021-04-14 23:24:37 +0300

    1. Бот [#475548] version 0.1

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -3,6 +3,7 @@ package ru.job4j.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
@@ -13,15 +14,23 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     String str;
+                    String message = " ";
                     while (!(str = in.readLine()).isEmpty()) {
                         System.out.println(str);
-                        if (str.contains("?msg=Bye")) {
-                            System.out.println("Server is closed!");
-                            out.write("Server is closed!\r\n\"".getBytes());
+                        if (str.contains("?msg=Exit")) {
+                            message = "Server is closed!";
+                            System.out.println(message);
                             server.close();
+                        } else if (str.contains("?msg=Hello")) {
+                            message = "Hello!";
+                            System.out.println(message);
+                        } else if (str.contains("?msg=")) {
+                            message = "What";
+                            System.out.println("What");
                         }
                     }
                     out.write("HTTP/1.1 200 OK\r\n\"".getBytes());
+                    out.write((message + "\r\n").getBytes());
                 }
             }
         }
