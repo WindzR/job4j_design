b56c98d0d5f171d17b3a88c2ac842be642e67cd9
WindzoR <windzr67@gmail.com>
2021-05-01 13:57:28 +0300

    1. Схема прав пользователи и роли. [#475454]

---
+++ data/create.sql
@@ -0,0 +1,54 @@
+create table users (
+id serial primary key,
+name varchar(255));
+
+create table roles (
+id serial primary key,
+role varchar(255),
+user_id int references users(id));
+
+create table rules(
+id serial primary key,
+rule varchar(255));
+
+create table rule_of_roles(
+id serial primary key,
+role_id int references roles(id),
+rule_id int references rules(id));
+
+create table items(
+id serial primary key,
+item varchar(255),
+id_user int references users(id),
+time date,	
+text text
+);
+
+create table comments(
+id serial primary key,
+time date,
+item_id int references items(id),
+text text
+);
+
+create table attachs(
+id serial primary key,
+path varchar(255),
+item_id int references items(id)
+);
+
+create table category(
+id serial primary key,
+category varchar(255),
+description text
+);
+
+alter table items add column category_id int references category(id);
+
+create table state(
+id serial primary key,
+status varchar(255)
+);
+
+alter table items add column state_id int references state(id);
+
